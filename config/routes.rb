93022f9297eff76b36e0ef1b39ba4ecfae435f91
lawrence <yuliang1987@gmail.com>
2014-09-08 21:16:55 +0800

    create new layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root :to => "home#index"
+
+  get '/about', to: 'static_pages#about'
+
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
