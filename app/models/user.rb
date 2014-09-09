706403a5ffd935d95656f08dba95b0c6c2f52bb4
lawrence <yuliang1987@gmail.com>
2014-09-10 00:16:01 +0800

    add validation for user and add error msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def to_param
+		self.name = name.downcase
+		name
+	end
+
 end
