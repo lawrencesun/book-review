14038ef82e30580cc69c668789e95ff298b25566
lawrence <yuliang1987@gmail.com>
2014-10-07 16:53:43 +0800

    relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :comments, dependent: :destroy
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationships",
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
   devise :database_authenticatable, :registerable, :confirmable,
@@ -16,4 +22,18 @@ class User < ActiveRecord::Base
   :content_type => { :content_type => ["image/jpeg", "image/jpg", "image/gif", "image/png"] },
   :size => { :less_than => 5.megabytes }
   	
+  def feed
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end    
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+  end
 end
