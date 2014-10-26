0b5d9f18f295d3933d1e08e78d63234bd102f40e
lawrence <yuliang1987@gmail.com>
2014-10-26 09:49:12 +0800

    modify migration

--- db/migrate/20141017120818_add_commentableto_comments.rb
+++ db/migrate/20141017120818_add_commentableto_comments.rb
@@ -1,7 +1,7 @@
 class AddCommentabletoComments < ActiveRecord::Migration
   def change
   	remove_column :comments, :book_id
-  	create_table :comments do |t|
+  	change_table :comments do |t|
   		t.references :commentable, polymorphic: true
   	end
   end
