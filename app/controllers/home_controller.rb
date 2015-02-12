384a2db128ba7cb458b98b2aadcfa658c76b28fc
lawrence <yuliang1987@gmail.com>
2015-02-12 15:41:00 +0800

    Modify homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@books = Book.order('created_at DESC').limit(10)
+		@topics = Topic.order('created_at DESC').limit(10)
 		@comments = Comment.order('created_at DESC').limit(10)
 	end
 end
